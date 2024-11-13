377df7a266bdb661f1c235e2d1974cf7f4a5a72f
bitRot <panatheonofhercules@gmail.com>
2024-11-13 12:11:48 +0530

    Step 2 completed

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	url := flag.String("url", "", "The URL to request")
-	method := flag.String("method", "GET", "HTTP method (GET, POST, etc.)")
-	body := flag.String("body", "", "Body to sent with the outgoing request")
+	url := flag.String("u", "", "The URL to request")
+	method := flag.String("m", "GET", "HTTP method (GET, POST, etc.)")
+	body := flag.String("bd", "", "Body to sent with the outgoing request")
+	number_of_requests := flag.Int("n", 1, "Number of times to send the request")
 	flag.Parse()
+	for *number_of_requests > 0 {
+		make_request(method, url, body)
+		*number_of_requests--
+	}
+}
+
+func make_request(method, url, body *string) {
 	status, statusCode, Response := Decode_And_Fetch_Response(*method, *url, *body)
 	fmt.Printf("Status:-%v \n", status)
 	fmt.Printf("statusCode:-%v\n", statusCode)
