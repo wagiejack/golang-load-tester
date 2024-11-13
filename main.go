5becbed25f15ecc6945c03abeb048fc7222c107a
bitRot <panatheonofhercules@gmail.com>
2024-11-13 22:46:10 +0530

    Step 5 completed, application completed

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"sync"
 )
 
@@ -13,9 +14,10 @@ type Request_Metrics struct {
 	time_for_request   uint64
 	time_to_first_byte int
 	time_to_last_byte  int
+	request_body       string
 }
 
-var metrics []Request_Metrics
+var Metrics []Request_Metrics
 
 func main() {
 	url := flag.String("u", "", "The URL to request")
@@ -25,26 +27,104 @@ func main() {
 	concurrent_requests := flag.Int("c", 1, "Number of Concurrent requests to be sent")
 	flag.Parse()
 
+	ResponseCode_Count := make(map[int]int)
+
 	var wg sync.WaitGroup
 	for i := 0; i < *concurrent_requests; i++ {
 		wg.Add(1)
-		go make_request(method, url, body, number_of_requests, &wg)
+		go make_request(method, url, body, number_of_requests, &wg, &ResponseCode_Count)
 	}
 	wg.Wait()
 
-	fmt.Printf("successes: %v\n", successful_requests_counter)
-	fmt.Printf("failures: %v\n", failure_requests_counter)
+	fmt.Printf("successful request: %v\n", successful_requests_counter)
+	fmt.Printf("failure request: %v\n", failure_requests_counter)
+	fmt.Println()
+	fmt.Printf("Request count by Status Code:-\n")
+
+	for k, v := range ResponseCode_Count {
+		fmt.Printf(" %v:%v\n", k, v)
+	}
+
+	//Measuring Metrics
+	var min_TTFB int = math.MaxInt
+	var max_TTFB int = math.MinInt
+	var avg_TTFB int = 0
+	var min_TTLB int = math.MaxInt
+	var max_TTLB int = math.MinInt
+	var avg_TTLB int = 0
+
+	var min_req_time uint64 = math.MaxUint64
+	var max_req_time uint64 = 0
+	var avg_req_time uint64 = 0
+
+	for _, v := range Metrics {
+		min_TTFB = min(&min_TTFB, &v.time_to_first_byte)
+		max_TTFB = max(&max_TTFB, &v.time_to_first_byte)
+		avg_TTFB += (v.time_to_first_byte)
+		min_TTLB = min(&min_TTFB, &v.time_to_last_byte)
+		max_TTLB = max(&max_TTFB, &v.time_to_last_byte)
+		avg_TTLB += (v.time_to_last_byte)
+
+		min_req_time = min_uint64(&min_req_time, &v.time_for_request)
+		max_req_time = max_uint64(&max_req_time, &v.time_for_request)
+		avg_req_time += v.time_for_request
+	}
+
+	fmt.Printf("Total Request Time (s) (Min, Max, Mean).....: %v, %v, %v\nTime to First Byte (s) (Min, Max, Mean).....: %v, %v, %v\nTime to Last Byte (s) (Min, Max, Mean)......:%v, %v, %v", ms_to_seconds_uint64(&min_req_time), ms_to_seconds_uint64(&max_req_time), ms_to_seconds_uint64(&avg_req_time)/float64(len(Metrics)), ms_to_seconds(&min_TTFB), ms_to_seconds(&max_TTFB), ms_to_seconds(&avg_TTFB)/float64(len(Metrics)), ms_to_seconds(&min_TTLB), ms_to_seconds(&max_TTLB), ms_to_seconds(&avg_TTLB)/float64(len(Metrics)))
+
 }
 
-func make_request(method, url, body *string, number_of_requests *int, wg *sync.WaitGroup) {
+func make_request(method, url, body *string, number_of_requests *int, wg *sync.WaitGroup, ResponseCode_Count *map[int]int) {
 	defer (*wg).Done()
 	for *number_of_requests > 0 {
+		//Making the Request
 		_, statusCode, _ := Decode_And_Fetch_Response(*method, *url, *body)
+
+		//Checking if request is successful or not and recording metrics accordingly
 		if statusCode >= 200 && statusCode < 300 {
 			successful_requests_counter++
 		} else {
 			failure_requests_counter++
 		}
+
+		//Incrementing StatusCode count on map for metrics
+		(*ResponseCode_Count)[statusCode]++
 		*number_of_requests--
 	}
+}
+
+func min(a *int, b *int) int {
+	if *a <= *b {
+		return *a
+	}
+	return *b
+}
+
+func max(a *int, b *int) int {
+	if *a <= *b {
+		return *b
+	}
+	return *a
+}
+
+func min_uint64(a *uint64, b *uint64) uint64 {
+	if *a <= *b {
+		return *a
+	}
+	return *b
+}
+
+func max_uint64(a *uint64, b *uint64) uint64 {
+	if *a <= *b {
+		return *b
+	}
+	return *a
+}
+
+func ms_to_seconds_uint64(v *uint64) float64 {
+	return float64(*v) / 1000
+}
+
+func ms_to_seconds(v *int) float64 {
+	return float64(*v) / 1000
 }
--- requests_and_responses.go
+++ requests_and_responses.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 // Expecting to extract the method and the url from the string that the user enters with all the validations that are done over here
@@ -36,35 +37,79 @@ func Decode_And_Fetch_Response(method, url, body string) (string, int, string) {
 }
 
 func GET_Response(url string) string {
+	//Recording time of starting
+	startTime := time.Now()
+
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln("error getting the response as", err.Error())
 	}
+	//TTFB
+	time_to_first_byte := time.Since(startTime)
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln("error reading response body as", err.Error())
 	}
 	final_body := string(body)
+
+	//TTLB
+	time_to_last_byte := time.Since(startTime)
+
+	//Storing the metrics
+	localMetric := Request_Metrics{
+		time_for_request:   uint64(time.Since(startTime)),
+		time_to_first_byte: int(time_to_first_byte),
+		time_to_last_byte:  int(time_to_last_byte),
+		request_body:       final_body,
+	}
+	Metrics = append(Metrics, localMetric)
 	return final_body
 }
 
 func POST_Response(url string, body string) string {
+
+	//Recording time of starting
+	startTime := time.Now()
+
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		log.Fatalln("Error sending the post response as ", err.Error())
 	}
+
+	//TTFB
+	time_to_first_byte := time.Since(startTime)
+
 	defer resp.Body.Close()
 	final_body := string(body)
+
+	//TTLB
+	time_to_last_byte := time.Since(startTime)
+
+	//Storing the metrics
+	localMetric := Request_Metrics{
+		time_for_request:   uint64(time.Since(startTime)),
+		time_to_first_byte: int(time_to_first_byte),
+		time_to_last_byte:  int(time_to_last_byte),
+		request_body:       final_body,
+	}
+	Metrics = append(Metrics, localMetric)
 	return final_body
 }
 
 func Create_Request_And_Send(method string, url string, body string) (string, int, string) {
+
+	//Recording time of starting
+	startTime := time.Now()
+
 	//Creation of Request
 	req, err := http.NewRequest(strings.ToUpper(method), url, bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		log.Fatalln("Error sending the post response as ", err.Error())
 	}
 	req.Header.Set("Content-Type", "application/json")
+
+	//TTFB
+	time_to_first_byte := time.Since(startTime)
 
 	//Create client send request
 	client := &http.Client{}
@@ -81,6 +126,19 @@ func Create_Request_And_Send(method string, url string, body string) (string, in
 	if err != nil {
 		log.Fatalf("Error reading response body: %v", err)
 	}
+
 	responseBody := string(bodyBytes)
+
+	//TTLB
+	time_to_last_byte := time.Since(startTime)
+	//Storing the metrics
+	localMetric := Request_Metrics{
+		time_for_request:   uint64(time.Since(startTime)),
+		time_to_first_byte: int(time_to_first_byte),
+		time_to_last_byte:  int(time_to_last_byte),
+		request_body:       responseBody,
+	}
+
+	Metrics = append(Metrics, localMetric)
 	return status, statusCode, responseBody
 }
